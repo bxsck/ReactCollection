cdf290082f083039142b55834c2a0c292c5d094d
bxsck <bosstk05nya@gmail.com>
2020-11-22 00:01:57 +0700

    more connect to store

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -11,7 +11,7 @@ class Button extends React.Component {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {value => this.renderSubmit(value)}
+          {({language}) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
